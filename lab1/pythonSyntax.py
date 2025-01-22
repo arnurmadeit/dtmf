c5329c949e1d52c737ee1caea3453025858311e4
Arnur! <arnipelmesh@gmail.com>
2025-01-22 13:41:23 +0500

    Update pythonSyntax.py

--- lab1/pythonSyntax.py
+++ lab1/pythonSyntax.py
@@ -1,3 +1,4 @@
 if 5 > 2:
   print("Five is greater than two!") 
 #Listen to DtMF by Bad Bunny
+#We also have to have the same amount of spaces in a block of code, using a different amount of spaces would break the code. 
