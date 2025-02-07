88f78a8774771eb8507088f7da533b6e60994ae0
Arnur! <arnipelmesh@gmail.com>
2025-02-07 15:25:16 +0500

    Create pythonOperators.py

---
+++ lab2/pythonOperators.py
@@ -0,0 +1,10 @@
+""" 
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+""" 
+#mind the precedence order! parentheses have the highest order
