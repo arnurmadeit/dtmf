084faa7e04d6b656e4fa0ccc36a0f6bdebcf41b4
Arnur! <arnipelmesh@gmail.com>
2025-02-07 21:23:50 +0500

    Create pythonForLoops.py

---
+++ lab2/pythonForLoops.py
@@ -0,0 +1,4 @@
+# A for loop is used to iterate over a sequence (e.g., a list, tuple, string, or range). It’s typically used when the number of iterations is known in advance.
+fruits = ['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)
