f7726bffca38785c8b88947745b2df974c679587
Arnur! <arnipelmesh@gmail.com>
2025-02-07 15:22:24 +0500

    Create pythonBooleans.py

---
+++ lab2/pythonBooleans.py
@@ -0,0 +1,6 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#Almost any value is evaluated to True if it has some sort of content.
+#In fact, there are not many values that evaluate to False, except empty values, such as (), [], {}, "", the number 0, and the value None. And of course the value False evaluates to False.
