10bf1cd0cdf893f66d51ebe06ef4886094e9d168
Arnur! <arnipelmesh@gmail.com>
2025-02-07 21:15:48 +0500

    Create pythonDictionaries.py

---
+++ lab2/pythonDictionaries.py
@@ -0,0 +1,4 @@
+# Dictionaries are key-value pair collections, where each key maps to a corresponding value. Keys must be unique and immutable (e.g., strings, numbers), but values can be any data type.
+# reminds me of map in c++
+my_dict = {'name': 'Alice', 'age': 25}
+print(my_dict['name'])  # Outputs: Alice
