23aa16fc94c5d8145eb5f5601d7ea07adbfd1b80
Arnur! <arnipelmesh@gmail.com>
2025-02-07 21:12:46 +0500

    Create pythonSets.py

---
+++ lab2/pythonSets.py
@@ -0,0 +1,3 @@
+#Sets are collections of unordered, unique elements. They are mutable (modifiable) but only store immutable objects.
+my_set = {1, 2, 3, 4, 4}
+print(my_set)  # Outputs: {1, 2, 3, 4}
